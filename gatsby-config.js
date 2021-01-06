0db8034e6cd1facfd2cbc0a595d6bdbf4bbdca40
Ola Vea <ola@olavea.com>
2021-01-06 13:45:30 +0100

    .env is not working #1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,29 @@
+// import dotenv from 'dotenv';
+
+// dotenv.config({ path: '.env'});
+
+// require("dotenv").config({
+//   path: `.env`,
+// })
+
+// {
+//   resolve: `gatsby-plugin-env-variables`,
+//   options: {
+//     allowList: ["GATSBY_CONTENTFUL_SPACE_ID", "GATSBY_CONTENTFUL_ACCESS_TOKEN"]
+//   },
+// },
+
 module.exports = {
   siteMetadata: {
     title: "SnoopyEars",
   },
   plugins: [
+
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "ANzd9ck9S3noeOnkPW8UvF_esMe6HVh8YT7R9etdxKU",
-        spaceId: "4zhx15eo3eqn",
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
       },
     },
     "gatsby-plugin-sharp",
